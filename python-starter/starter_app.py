fdbb79a9c1bd5058d97406e70ef4a9d551c55fd3
above-c-level <jtoftum@redhat.com>
2022-07-08 14:24:16 -0400

    update makefile and requirements

--- python-starter/starter_app.py
+++ python-starter/starter_app.py
@@ -1,162 +1,132 @@
-from kafka import KafkaProducer
-import random
+from re import I
 import time
-from app_common_python import LoadedConfig, isClowderEnabled
-import environ
-from os import listdir
+from datetime import datetime
 
+from app_common_python import LoadedConfig
+from confluent_kafka import Consumer, Producer
+from flask import Flask, jsonify, request
+from prometheus_client import Info, Summary, start_http_server, Counter, Gauge
 
-print("""\n\n
-##############################################################################
-                             Running starter app
-##############################################################################
-""")
-CLOWDER_ENABLED = isClowderEnabled()
-if CLOWDER_ENABLED:
-    print("\n😸\tClowder is enabled")
-else:
-    print("\n😿\tClowder is disabled")
+import clowder_info
 
-# I don't know why we're going up 3 directory levels, since
-# `ROOT_DIR = "/"` should work just fine I'd think?
-ROOT_DIR = environ.Path(__file__) - 3
-# print(f"ROOT_DIR: {ROOT_DIR}")
-
+clowder_info.print_info()
+APP = Flask(__name__)
+KAFKA_SERVER = clowder_info.KAFKA_SERVER
+CONSUMER = Consumer({
+    "bootstrap.servers": KAFKA_SERVER,
+    "group.id": __name__,
+    #  "auto.offset.reset": "earliest"
+})
+UNCONSUMED_MESSAGES = Gauge("unconsumed_messages",
+                            "Number of unconsumed messages")
+CONSUMED_MESSAGES = Counter("consumed_messages", "Number of consumed messages")
+PRODUCED_MESSAGES = Counter("produced_messages", "Number of produced messages")
+HEALTH_CALLS = Counter("health_calls", "Number of health calls")
 
 if LoadedConfig is None:
-    raise ValueError("\n🚫\tLoadedConfig is None, impossible to continue")
+    raise ValueError("LoadedConfig is None, impossible to continue")
 
-ENVIRONMENT = environ.Env()
-
-print("\n✍️\tKafka:")
-if CLOWDER_ENABLED:
-    if LoadedConfig.kafka is None:
-        print("\t  🚫 LoadedConfig.kafka is None")
-    else:
-        kafka_broker = LoadedConfig.kafka.brokers[0]
-        KAFKA_HOST = kafka_broker.hostname
-        KAFKA_PORT = kafka_broker.port
-        KAFKA_SERVER = f"{KAFKA_HOST}:{KAFKA_PORT}"
-        print(f"\t▪ KAFKA_SERVER: {KAFKA_SERVER}")
-else:
-    KAFKA_HOST = "localhost"
-    KAFKA_PORT = "9092"
-    KAFKA_SERVER = f"{KAFKA_HOST}:{KAFKA_PORT}"
-    print(f"\t▪ KAFKA_SERVER: {KAFKA_SERVER}")
-
-print("\n🗄️\tDatabase:")
-# Database stuff
-if LoadedConfig.database is None:
-    print("\t🚫 LoadedConfig.database is None")
-    db_options = {}
-elif LoadedConfig.database.rdsCa:
-    db_options = {
-        "OPTIONS": {
-            "sslmode": ENVIRONMENT.get_value("PGSSLMODE", default="prefer"),
-            "sslrootcert": LoadedConfig.rds_ca(),
-        }
-    }
-else:
-    db_options = {}
-print(f"\t▪ db_options: {db_options}")
-
-print("\n💾\tIn-memory db:")
-if CLOWDER_ENABLED:
-    if LoadedConfig.inMemoryDb is None:
-        print("\t🚫 LoadedConfig.inMemoryDb is None")
-    else:
-        REDIS_HOST = LoadedConfig.inMemoryDb.hostname
-        REDIS_PORT = LoadedConfig.inMemoryDb.port
-        DEFAULT_REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
-        print(f"\t▪ Default redis url: {DEFAULT_REDIS_URL}")
-else:
-    REDIS_HOST = ENVIRONMENT.get_value("REDIS_HOST", default="localhost")
-    REDIS_PORT = ENVIRONMENT.get_value("REDIS_PORT", default="6379")
-    DEFAULT_REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
-    print(f"\t▪ Default redis url: {DEFAULT_REDIS_URL}")
+print(f"\n\n🚀\tStarted at: {datetime.now()}\n")
 
 
-
-print("\n☁️\tCloudWatch:")
-# CW settings
-if CLOWDER_ENABLED:
-    if ENVIRONMENT.bool("CW_NULL_WORKAROUND", default=True):
-        CW_AWS_ACCESS_KEY_ID = None
-        CW_AWS_SECRET_ACCESS_KEY = None
-        CW_AWS_REGION_NAME = None
-        CW_LOG_GROUP = None
-        print(f"\t▪ CW_AWS_ACCESS_KEY_ID: {CW_AWS_ACCESS_KEY_ID}")
-        print(f"\t▪ CW_AWS_SECRET_ACCESS_KEY: {CW_AWS_SECRET_ACCESS_KEY}")
-        print(f"\t▪ CW_AWS_REGION_NAME: {CW_AWS_REGION_NAME}")
-        print(f"\t▪ CW_LOG_GROUP: {CW_LOG_GROUP}")
-    elif LoadedConfig.logging is None:
-        print("\t🚫 LoadedConfig.logging is None")
-    elif LoadedConfig.logging.cloudwatch is None:
-        print("\t🚫 LoadedConfig.logging.cloudwatch is None")
-    else:
-        CW_AWS_ACCESS_KEY_ID = LoadedConfig.logging.cloudwatch.accessKeyId
-        CW_AWS_SECRET_ACCESS_KEY = LoadedConfig.logging.cloudwatch.secretAccessKey
-        CW_AWS_REGION_NAME = LoadedConfig.logging.cloudwatch.region
-        CW_LOG_GROUP = LoadedConfig.logging.cloudwatch.logGroup
-        print(f"\t▪ CW_AWS_ACCESS_KEY_ID: {CW_AWS_ACCESS_KEY_ID}")
-        print(f"\t▪ CW_AWS_SECRET_ACCESS_KEY: {CW_AWS_SECRET_ACCESS_KEY}")
-        print(f"\t▪ CW_AWS_REGION_NAME: {CW_AWS_REGION_NAME}")
-        print(f"\t▪ CW_LOG_GROUP: {CW_LOG_GROUP}")
-else:
-    print("\t🚫 Clowder is disabled")
-    CW_AWS_ACCESS_KEY_ID = ENVIRONMENT.get_value("CW_AWS_ACCESS_KEY_ID",
-                                                 default=None)
-    CW_AWS_SECRET_ACCESS_KEY = ENVIRONMENT.get_value(
-        "CW_AWS_SECRET_ACCESS_KEY", default=None)
-    CW_AWS_REGION = ENVIRONMENT.get_value("CW_AWS_REGION", default="us-east-1")
-    CW_LOG_GROUP = ENVIRONMENT.get_value("CW_LOG_GROUP",
-                                         default="platform-dev")
-    print(f"\t▪ CW_AWS_ACCESS_KEY_ID: {CW_AWS_ACCESS_KEY_ID}")
-    print(f"\t▪ CW_AWS_SECRET_ACCESS_KEY: {CW_AWS_SECRET_ACCESS_KEY}")
-    print(f"\t▪ CW_AWS_REGION: {CW_AWS_REGION}")
-    print(f"\t▪ CW_LOG_GROUP: {CW_LOG_GROUP}")
-
-CW_CREATE_LOG_GROUP = ENVIRONMENT.bool("CW_CREATE_LOG_GROUP", default=False)
-
-# If clowder is enabled we should grab the s3 connection info in addition
-print("\n📦\tMINIO:")
-if LoadedConfig.objectStore is None:
-    print("\t🚫 LoadedConfig.objectStore is None")
-else:
-    MINIO_HOSTNAME = LoadedConfig.objectStore.hostname
-    MINIO_PORT = LoadedConfig.objectStore.port
-    MINIO_TLS = LoadedConfig.objectStore.tls
-    MINIO_ACCESSKEY = LoadedConfig.objectStore.accessKey
-    MINIO_SECRETKEY = LoadedConfig.objectStore.secretKey
-    MINIO_SERVER = f"{MINIO_HOSTNAME}:{MINIO_PORT}"
-    print(f'\t▪ Minio server: {MINIO_SERVER}')
-    print(f'\t▪ Minio access key: {MINIO_ACCESSKEY}')
-    print(f'\t▪ Minio secret key: {MINIO_SECRETKEY}')
-    print(f'\t▪ Minio tls: {MINIO_TLS}')
+def start_prometheus():
+    # if Clowder is enabled then prometheus is always set
+    start_http_server(port=clowder_info.PROMETHEUS_PORT)
 
 
-print("\n🐷\tThat's all, folks!")
-# producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-# for _ in range(20):
-#     num = random.randint(0, 100)
-#     numstr = str(num).encode('utf-8')
-#     producer.send('quickstart-events', bytes(numstr))
-#     time.sleep(1)
+@APP.route('/livez', methods=['GET', 'PUT', 'POST'])
+def liveness():
+    print("In liveness()")
+    return "In liveness()"
 
-# 0. Get django running for simple API to send/receive messages through kafka
-# 0.1. Do file stuff through django
-# 1. dummy API for liveness and readiness probes
+
+@APP.route('/readyz', methods=['GET', 'PUT', 'POST'])
+def readiness():
+    print("In readiness()")
+    return 'In readiness()'
+
+
+@APP.route('/healthz', methods=['GET', 'PUT', 'POST'])
+def health():
+    HEALTH_CALLS.inc()
+    print("In health()")
+    return 'In health()'
+
+
+@APP.route('/kafka', methods=['GET'])
+def kafka_get():
+    CONSUMER.subscribe([topic.name for topic in LoadedConfig.kafka.topics])
+    messages = {}
+    start = time.time()
+    # For some reason Kafka will sometimes return 0 messages, so if we consume
+    # a couple times until something shows up, we should get messages
+    #? Is there a better way to do this?
+    while time.time() < start + 3:
+        # Let's consume upwards of 5000 messages
+        current_messages = CONSUMER.consume(num_messages=5000, timeout=0.5)
+        for message in current_messages:
+            # message.timestamp() might be useful as well
+            topic, value = message.topic(), message.value().decode('utf-8')
+            if topic not in messages:
+                messages[topic] = []
+            messages[topic].append(value)
+        if len(messages) > 0:
+            break
+    CONSUMER.unsubscribe()
+    CONSUMED_MESSAGES.inc()
+    UNCONSUMED_MESSAGES.dec()
+    return messages
+
+
+@APP.route('/kafka', methods=['PUT', 'POST'])
+def kafka_put():
+    producer = Producer({
+        "bootstrap.servers": KAFKA_SERVER,
+        "client.id": __name__
+    })
+    request_data = request.get_data()
+    #? Get topic from the request?
+    # LoadedConfig.kafka.topics contains a list of topics, maybe just pick one?
+    producer.produce('test-topic', request_data)
+    producer.flush()
+    PRODUCED_MESSAGES.inc()
+    UNCONSUMED_MESSAGES.inc()
+    return f"In kafka_put() with request_data: {request_data}\n"
+
+
+# @APP.route('/')
+# def root():
+#     print("In root()")
+#     return 'In root()'
+
+
+# 0. dummy API for liveness and readiness probes ✅ (apparently clowder built-in)
+# 1. Get simple API to send/receive messages through kafka ✅
 # 2. get examples for each clowder provider:
-#        In-memory db ✅
+#        Kafka - Finished, sort of ✅
+#              - Has a bug 🐛 (possibly in Flask?) The issue is that sometimes
+#                the GET method returns {} instead of the messages we want
+#        Web - Finished ✅
+#        In-memory db - In progress 🔄
+#        Postgres - In progress 🔄
+#        Minio - In progress 🔄
+#        Metrics - In progress 🔄
+#        InitContainer
 #        CronJob
 #        CJI
-#        Feature Flags
-#        Web
-#        Metrics
-#        InitContainer
+#        Feature Flags - Unleash
 # 3. Eventually be able to `oc process`/`oc apply` the starter app
-
+if __name__ == '__main__':
+    #? Figure out what's going on with prometheus:
+    #? OSError: [Errno 98] Address already in use
+    #? Presumably something else is already running on port 9000
+    #? start_prometheus()
+    PORT = LoadedConfig.publicPort
+    print(f"PORT: {PORT}")
+    APP.run(host='0.0.0.0', port=PORT, debug=True)
 # Build stuff
 # Push to quay
-while True:
-    time.sleep(1)
+
+# oc get service
+# oc port-forward svc/starterapp-worker-service 8000
+# oc port-forward svc/env-boot-minio 9000
