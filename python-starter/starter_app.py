9b62ec77ee5590838a02b654b32fa2e10542ccaa
above-c-level <jtoftum@redhat.com>
2022-07-18 17:56:20 -0400

    Added redis before realizing I was on wrong branch

--- python-starter/starter_app.py
+++ python-starter/starter_app.py
@@ -1,11 +1,13 @@
-import time
-import random
-from datetime import datetime
 import os
+import random
+import threading
+from datetime import datetime
 
-from app_common_python import LoadedConfig, KafkaTopics
+import psycopg2
+import redis
+from app_common_python import KafkaTopics, LoadedConfig
 from confluent_kafka import Consumer, Producer
-from flask import Flask, request
+from flask import Flask, make_response, request
 from minio import Minio
 from prometheus_client import Counter, Gauge, start_http_server
 
@@ -15,12 +17,14 @@ clowder_info.print_info()
 
 print(f"\n\n🚀\tStarted at: {datetime.now()}\n")
 
+if LoadedConfig is None:
+    raise ValueError("LoadedConfig is None, impossible to continue")
+
 APP = Flask(__name__)
 KAFKA_SERVER = clowder_info.KAFKA_SERVER
 CONSUMER = Consumer({
     "bootstrap.servers": KAFKA_SERVER,
     "group.id": __name__
-    #  "auto.offset.reset": "earliest"
 })
 
 # This just gets the first key it can and uses that
@@ -30,9 +34,6 @@ UNCONSUMED_MESSAGES = Gauge("unconsumed_messages",
 CONSUMED_MESSAGES = Counter("consumed_messages", "Number of consumed messages")
 PRODUCED_MESSAGES = Counter("produced_messages", "Number of produced messages")
 HEALTH_CALLS = Counter("health_calls", "Number of health calls")
-
-if LoadedConfig is None:
-    raise ValueError("LoadedConfig is None, impossible to continue")
 
 if LoadedConfig.objectStore is not None:
     MINIO_CLIENT = Minio(clowder_info.MINIO_SERVER,
@@ -45,16 +46,33 @@ if LoadedConfig.objectStore is not None:
 else:
     MINIO_ENABLED = False
 
+REDIS_CONN = redis.Redis(host=clowder_info.REDIS_HOST,
+                         port=clowder_info.REDIS_PORT)
+
+# TODO: Add checking to this
+# pg_options = clowder_info.db_options["OPTIONS"]
+# POSTGRES_CONN = psycopg2.connect(
+#     host=pg_options['hostname'],
+#     port=pg_options['port'],
+#     database="suppliers",
+#     user="postgres",
+#     password="Abcd1234"
+#     )
+
 
 def start_prometheus():
     # if Clowder is enabled then prometheus is always set
     print(f"Metrics port: {clowder_info.METRICS_PORT}")
     start_http_server(port=clowder_info.METRICS_PORT)
+    print("Prometheus server started")
 
 
 # Generate a basic file to be used as a test file, and get them all
 @APP.route('/minio', methods=['GET'])
 def minio_get():
+    """
+    Handles GET requests to the /minio endpoint. Returns object data from Minio.
+    """
     print("In minio_get")
     if not MINIO_ENABLED:
         return "Minio is not enabled"
@@ -70,6 +88,11 @@ def minio_get():
 
 @APP.route('/minio', methods=['POST', 'PUT'])
 def minio_put():
+    """
+    Handles PUT and POST requests to the /minio endpoint. Adds example files
+    to an example bucket called `testbucket` with the file name as the timestamp
+    and the file contents as a string of random bits.
+    """
     print("In minio_put")
     if not MINIO_ENABLED:
         return "Minio is not enabled"
@@ -82,6 +105,47 @@ def minio_put():
                              f"{current_time}.txt")
     # Delete the temp file
     os.remove(f"{current_time}.txt")
+    return "OK"
+
+
+@APP.route('/redis', methods=['GET'])
+def redis_get():
+    """
+    Handles GET requests to the /redis endpoint. Returns the value associated
+    with the specified key. If the key does not exist, returns a 404.
+
+    `key` is a required input to the request.
+    """
+    print("In redis_get()")
+    key = request.args.get("key")
+    if key is None:
+        return make_response("No key specified", 400)
+    result = REDIS_CONN.get(key)
+    print(f"Redis result: {result}")
+    if result is None:
+        return make_response("Key not found in database", 404)
+    return result
+
+
+@APP.route('/redis', methods=['POST', 'PUT'])
+def redis_put():
+    """
+    Handles PUT and POST requests to the /redis endpoint. Adds a message to
+    the specified key in the Redis database. If the key already exists, it is
+    overwritten.
+
+    `key` and `value` are required inputs to the request.
+    """
+    print("In redis_put()")
+    key = request.args.get("key")
+    if key is None:
+        return make_response("No key provided", 400)
+    value = request.args.get("value")
+    if value is None:
+        return make_response("No value provided", 400)
+    result = REDIS_CONN.set(key, value)
+    if not result:
+        return make_response("Failed to set properly", 500)
     return "OK"
 
 
@@ -104,31 +168,42 @@ def health():
     return 'In health()'
 
 
-@APP.route('/kafka', methods=['GET'])
-def kafka_get():
-    consumer_topics = list(KafkaTopics)
-    print(f"Consumer topics: {consumer_topics}")
-    CONSUMER.subscribe(consumer_topics)
-    messages = {}
-    # I talked with Chris and got this working, the issue is essentially that
-    # the consumption only occurs while *in* this function, so we need another
-    # way of getting messages, i.e. a thread
-    print("Starting message consumption")
-    current_messages = CONSUMER.consume(num_messages=500, timeout=10)
-    print(f"Current Messages: {current_messages}")
-    for message in current_messages:
-        print(f"\t\tMessage: {message}")
+MESSAGES = {}
+
+
+# TODO: Replace this function with one that pushes to Postgres
+# That would be much safer than this temp approach
+def consume_messages():
+    global MESSAGES
+    CONSUMER.subscribe(list(KafkaTopics))
+    while True:
+        messages = MESSAGES.copy()
+        msg = CONSUMER.poll(timeout=1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"Consumer error: {msg.error()}")
+            continue
         # message.timestamp() might be useful as well
-        topic, value = message.topic(), message.value().decode('utf-8')
+        topic, value = msg.topic(), msg.value().decode('utf-8')
         if topic not in messages:
             messages[topic] = []
         messages[topic].append(value)
-    print("Out of consumption loop")
-    CONSUMER.unsubscribe()
-    CONSUMED_MESSAGES.inc()
-    UNCONSUMED_MESSAGES.dec()
-    print(f"Messages: {messages}")
-    return messages
+        MESSAGES = messages
+        UNCONSUMED_MESSAGES.dec()
+        CONSUMED_MESSAGES.inc()
+        print(f"  Consumed message: {msg.value()}")
+
+
+# TODO: Replace this function with one that pulls from Postgres
+# complementary to the consume_messages() function
+@APP.route('/kafka', methods=['GET'])
+def kafka_get():
+    global MESSAGES
+    print(f"Copying messages {MESSAGES}")
+    messages_copy = MESSAGES.copy()
+    MESSAGES = {}
+    return messages_copy
 
 
 @APP.route('/kafka', methods=['PUT', 'POST'])
@@ -138,8 +213,8 @@ def kafka_put():
         "client.id": __name__
     })
     request_data = request.get_data()
-    print(f"Produced {request_data} on topic: '{PRODUCER_TOPIC}'")
     producer.produce(PRODUCER_TOPIC, request_data)
+    print(f"Produced {request_data} on topic: '{PRODUCER_TOPIC}'")
     producer.flush()
     PRODUCED_MESSAGES.inc()
     UNCONSUMED_MESSAGES.inc()
@@ -153,12 +228,12 @@ def kafka_put():
 
 # 0. dummy API for liveness and readiness probes ✅ (apparently clowder built-in)
 # 1. Get simple API to send/receive messages through kafka ✅
-#  - At some point, I need to convert from Flask to Django 
+#  - At some point, I need to convert from Flask to Django
 # 2. get examples for each clowder provider:
-#        Kafka - In progress 🔄
 #        Web - Finished ✅
 #        Minio - In progress ✅
-#        In-memory db - In progress 🔄
+#        In-memory db - In progress ✅
+#        Kafka - In progress 🔄
 #        Postgres - In progress 🔄
 #        Metrics - In progress 🔄
 #        InitContainer
@@ -166,15 +241,24 @@ def kafka_put():
 #        CJI
 #        Feature Flags - Unleash
 # 3. Eventually be able to `oc process`/`oc apply` the starter app
+
+# Current goals:
+# - Figure out connection to postgres
+#   It looks like insights-core, vmaas, and insights-host-inventory all have
+#   references to psycopg2
+# - Get example of redis
 if __name__ == '__main__':
-    # ? Clowder defaults to port 9000 for metrics, but this is raised:
+    # We need to have a thread for consume_messages() to run in the background
+    CONSUMER_THREAD = threading.Thread(target=consume_messages)
+    CONSUMER_THREAD.start()
     # ? OSError: [Errno 98] Address already in use
-    # ? How should we deal with the conflict?
-    # ? Reach out to Pete
+    # ? ONLY shows up when APP.run() is also called, despite the fact that the
+    # ? call is done *after* the start_prometheus() call???
     # start_prometheus()
     PORT = LoadedConfig.publicPort
     print(f"public port: {PORT}")
-    APP.run(host='0.0.0.0', port=PORT, debug=True)
+    APP.run(host='0.0.0.0', port=PORT, debug=True, threaded=True)
+
 # Build stuff
 # Push to quay
 
